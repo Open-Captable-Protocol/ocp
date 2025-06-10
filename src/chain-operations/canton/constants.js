3c865225609eaef7fd07d75c1f9d7a638fdfdf70
HardlyDifficult <hardlydiff@gmail.com>
2025-06-10 16:35:04 -0400

    CAIP-7 TODO

--- src/chain-operations/canton/constants.js
+++ src/chain-operations/canton/constants.js
@@ -1,3 +1,6 @@
+// TODO https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-7.md
+// Once changed from chainId: number to CAIP-7: string, use `canton:mainnet` and `canton:devnet`
+
 export const CANTON_MAINNET_CHAIN_ID = 6765788401;
 export const CANTON_DEVNET_CHAIN_ID = 6765788402;
 
